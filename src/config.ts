ec7b8f1c8ceb4b2385b72742a136d455db1ff7d9
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-10-12 21:39:23 +0700

    update config live notif

--- src/config.ts
+++ src/config.ts
@@ -16,32 +16,34 @@ const config: ShowroomWatcherOptions = {
     no_activity_refresh: 60000
   },
   discord: {
-    channel_name: 'showroom', // if NODE_ENV=development channel name will be suffixed with "-dev"
+    channel_name: '🔔︱live-notification', // if NODE_ENV=development channel name will be suffixed with "-dev"
     specific_guilds: []
   },
   message: {
     live: (watcher: WatcherData): EmbedBuilder => {
+      const name = `${watcher.roomKey.replace('JKT48_', '')} JKT48`
       const giftData = watcher.giftLog.getTop()
       return new EmbedBuilder()
-        .setTitle(watcher.name)
+        .setTitle(`${name} lagi live showroom nih!`)
         .setDescription(`Total Gift : \`${giftData?.total ?? 0}\`\n\`\`\`${giftData?.users || 'No gift right now'}\`\`\``)
         .setImage(watcher.image)
         .setColor(liveColor)
-        .setFooter({ text: 'Showroom Gift Counter', iconURL: 'https://play-lh.googleusercontent.com/AKKnVMfKzyuPK2QLY3WDhhfBKyhW8nq5KjYARLcDgcub4ue5B9RSBzjv4-nU5EA1debu' })
-        .setURL(`https://www.showroom-live.com/${watcher.roomKey}`)
+        .setFooter({ text: 'Showroom Gift Counter', iconURL: 'https://www.jkt48showroom.com/logo.png' })
+        .setURL(`https://www.jkt48showroom.com/room/${watcher.roomKey}/${watcher.id}?type=live-notif`)
     },
     live_end: (watcher: WatcherData): EmbedBuilder => {
+      const name = `${watcher.roomKey.replace('JKT48_', '')} JKT48`
       const giftData = watcher.giftLog.getTop()
       return new EmbedBuilder()
-        .setTitle(watcher.name)
+        .setTitle(name)
         .setDescription(
-          `Live ${watcher.name} is finished!` + `\nTotal Gifts : \`${giftData?.total ?? 0}\``
+          `Live ${watcher.name} telah selesai!` + `\nTotal Gifts : \`${giftData?.total ?? 0}\`` + `\nViewers : \`${watcher?.penonton.peak ?? 0}\``
         )
         .setImage(watcher.image)
         .setColor(finishColor)
         .setTimestamp(new Date().getTime())
-        .setFooter({ text: 'Showroom', iconURL: 'https://play-lh.googleusercontent.com/AKKnVMfKzyuPK2QLY3WDhhfBKyhW8nq5KjYARLcDgcub4ue5B9RSBzjv4-nU5EA1debu' })
-        .setURL(`https://www.showroom-live.com/${watcher.roomKey}`)
+        .setFooter({ text: 'Showroom', iconURL: 'https://www.jkt48showroom.com/logo.png' })
+        .setURL(`https://www.jkt48showroom.com/room/${watcher.roomKey}/${watcher.id}?type=live-notif`)
     }
   }
 
